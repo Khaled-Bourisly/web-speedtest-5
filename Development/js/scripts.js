3655aa05b9e223d646a15bf37cbf729403e0c931
Khaled-Bourisly <bourislyk00@gmail.com>
2020-07-01 17:00:48 +0300

    speed5

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,11 +1,11 @@
-let instructor = {
-  name: "Menawer",
-  courses: ["JavaScript", "React", "PHP"],
-  githubAccount: {
-    username: "careless10",
-    numberOfRepos: 200,
-  },
-  sayHello: function () {
+let instructor = (console.log)
+  name: ("Menawer");
+  courses:("JavaScript");
+  githubAccount=console.log()
+    username: ("careless10");
+    numberOfRepos: 200;
+  
+  sayHello: function $() {
     console.log("Hi, I'm Menawer");
-  },
-};
+  }
+
